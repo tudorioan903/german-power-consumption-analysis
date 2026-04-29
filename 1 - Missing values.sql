704b22ade3da8b4f29ecd781f15de4a708285729
tudorioan903 <tudorioan903@yahoo.ro>
2026-04-29 20:33:40 +0300

    Delete 1 - Missing values.sql

--- 1 - Missing values.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    COUNT(*) as total_rows,
-    COUNT(load_actual_load) as non_null_load
-FROM power_consumption_germany;
-
-SELECT *
-FROM power_consumption_germany
-WHERE load_actual_load IS NULL;
