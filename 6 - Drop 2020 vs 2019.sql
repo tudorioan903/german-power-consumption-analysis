ad5e3b4a7396f567623d005c5053cf16d7007e0e
tudorioan903 <tudorioan903@yahoo.ro>
2026-04-29 20:34:31 +0300

    Delete 6 - Drop 2020 vs 2019.sql

--- 6 - Drop 2020 vs 2019.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    AVG(CASE WHEN year = 2019 THEN load_actual_load END) as avg_2019,
-    AVG(CASE WHEN year = 2020 THEN load_actual_load END) as avg_2020,
-    (
-        AVG(CASE WHEN year = 2020 THEN load_actual_load END) -
-        AVG(CASE WHEN year = 2019 THEN load_actual_load END)
-    ) * 100.0 /
-    AVG(CASE WHEN year = 2019 THEN load_actual_load END) as pct_change
-FROM clean_energy;
